e8d469cc4cbfab46380bd48c8c59740a355ab160
Maxim Romanov <romanov.maxim@gmail.com>
2020-07-22 14:18:07 +0200

    order of languages fixed

--- scripts/GenerateLanguagesInfos.py
+++ scripts/GenerateLanguagesInfos.py
@@ -18,13 +18,14 @@ print(langcode_to_langname)
 
 with open('languages.csv', 'w') as f:
     csvwriter = csv.writer(f, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    onlyfiles = sorted(onlyfiles)
     for file in onlyfiles:
         if file.startswith("."):
             pass
         else:
             subfiles = [d for d in listdir(mypath + file)]
-            print(subfiles)
-            for sf in subfiles:
+            for sf in sorted(subfiles):
+                print(sf)
                 topics.add(sf[sf.rfind('_') + 1:])
             csvwriter.writerow([file, ','.join(list(topics)), langcode_to_langname[file]])
             # lang = file[0:file.rfind('_')]
