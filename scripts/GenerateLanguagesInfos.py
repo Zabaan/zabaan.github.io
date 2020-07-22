79a3bfd178ae26f76bca05b80b51c0ec4341a210
Maxim Romanov <romanov.maxim@gmail.com>
2020-07-22 13:58:30 +0200

    language collection updated

--- scripts/GenerateLanguagesInfos.py
+++ scripts/GenerateLanguagesInfos.py
@@ -19,17 +19,20 @@ print(langcode_to_langname)
 with open('languages.csv', 'w') as f:
     csvwriter = csv.writer(f, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     for file in onlyfiles:
-        subfiles = [d for d in listdir(mypath + file)]
-        print(subfiles)
-        for sf in subfiles:
-            topics.add(sf[sf.rfind('_') + 1:])
-        csvwriter.writerow([file, ','.join(list(topics)), langcode_to_langname[file]])
-        # lang = file[0:file.rfind('_')]
-        # print(lang)
-        # if lang in langs:
-        #     langs[lang].add(file[file.rfind('_') + 1:])
-        # else:
-        #     langs[lang] = set()
-        #     langs[lang].add(file[file.rfind('_') + 1:])
+        if file.startswith("."):
+            pass
+        else:
+            subfiles = [d for d in listdir(mypath + file)]
+            print(subfiles)
+            for sf in subfiles:
+                topics.add(sf[sf.rfind('_') + 1:])
+            csvwriter.writerow([file, ','.join(list(topics)), langcode_to_langname[file]])
+            # lang = file[0:file.rfind('_')]
+            # print(lang)
+            # if lang in langs:
+            #     langs[lang].add(file[file.rfind('_') + 1:])
+            # else:
+            #     langs[lang] = set()
+            #     langs[lang].add(file[file.rfind('_') + 1:])
 print(langs)
 #    csvwriter.writerow([onlyfiles, ','.join(list(topics))])
